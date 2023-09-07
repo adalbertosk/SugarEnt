569785a712ba972c90ab794a05ae4018d367daa6
Adalberto <adalbertosk@gmail.com>
2023-09-07 12:34:22 +0100

    Added new field

---
+++ sugar/custom/Extension/modules/Accounts/Ext/Language/en_us.lang.php
@@ -0,0 +1,4 @@
+<?php
+// WARNING: The contents of this file are auto-generated.
+$mod_strings['LBL_NEW_FIELD'] = 'New Field';
+$mod_strings['LBL_RECORDVIEW_PANEL1'] = 'New Panel 1';
---
+++ sugar/custom/Extension/modules/Accounts/Ext/Vardefs/sugarfield_new_field_c.php
@@ -0,0 +1,14 @@
+<?php
+ // created: 2023-09-07 12:10:53
+$dictionary['Account']['fields']['new_field_c']['labelValue']='New Field';
+$dictionary['Account']['fields']['new_field_c']['full_text_search']=array (
+  'enabled' => true,
+  'boost' => '1',
+  'searchable' => true,
+);
+$dictionary['Account']['fields']['new_field_c']['enforced']='';
+$dictionary['Account']['fields']['new_field_c']['dependency']='';
+$dictionary['Account']['fields']['new_field_c']['required_formula']='';
+$dictionary['Account']['fields']['new_field_c']['readonly_formula']='';
+
+ ?>
